306e3e0dec187cde95273b22c760d1fef72a1a31
FailCake <edunad@users.noreply.github.com>
2023-09-01 16:13:52 +0200

    [rawrbox] new port

---
+++ ports/rawrbox/portfile.cmake
@@ -0,0 +1,14 @@
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO edunad/rawrbox
+  REF f7f45877f39274eb337bb7a173a706437f1ee6bf
+  HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+  SOURCE_PATH "${SOURCE_PATH}"
+  PREFER_NINJA
+)
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets()
